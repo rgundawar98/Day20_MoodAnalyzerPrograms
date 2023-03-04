9b70656466770d951910aa1de92fb93dc9b56dc1
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-03-04 17:47:12 +0530

    Adding welcome message

---
+++ MoodAnalyzerProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome to the MoodAnalyzer Programs");
+        }
+    }
+}
